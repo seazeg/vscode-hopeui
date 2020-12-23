924ef05dc2e43240ff023e070ce413662da145ef
Evan.G <seazeg@126.com>
2020-12-23 18:10:04 +0800

    default Commit Message [Evan.G]

--- src/extension.js
+++ src/extension.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : Evan.G
  * @Date         : 2020-12-23 10:03:49
- * @LastEditTime : 2020-12-23 11:08:36
+ * @LastEditTime : 2020-12-23 18:07:57
  * @Description  :
  */
 
@@ -15,8 +15,8 @@ exports.activate = function (context) {
     console.log("恭喜，您的扩展“vscode-plugin”已被激活！");
     // 注册命令
     context.subscriptions.push(
-        vscode.commands.registerCommand("extension.sayHello", function (uri) {
-            vscode.window.showInformationMessage("路径：" + uri);
+        vscode.commands.registerCommand("ext.hopeui.init", function (uri) {
+            vscode.window.showInformationMessage('HopeUI');
         })
     );
 
