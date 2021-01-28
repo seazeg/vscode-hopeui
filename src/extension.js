da0370f7f53f6c26999c6cb7566edf75bad2263e
Evan.G <seazeg@126.com>
2021-01-28 18:09:28 +0800

    default Commit Message [Evan.G]

--- src/extension.js
+++ src/extension.js
@@ -1,9 +1,9 @@
 "use strict";
 /*
- * @Author       : Evan.G
- * @Date         : 2020-12-23 10:03:49
- * @LastEditTime : 2021-01-18 16:10:52
- * @Description  :
+ * @Author : Evan.G
+ * @Date : 2020-12-23 10:03:49
+ * @LastEditTime : 2021-01-28 16:52:28
+ * @Description :
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
@@ -11,18 +11,25 @@ const vscode = require("vscode");
 const treeView_1 = require("./treeView");
 const webView_1 = require("./webView");
 const LABEL_URI_MAP = new Map([
+    ["获取最新版", "概览-welcome-to-hopeui--welcome-to-hope-ui"],
     ["按钮(Button)", "基础控件-按钮-button--通用"],
     ["复选框(Checkbox)", "基础控件-复选框-checkbox--复选框-checkbox"],
     ["文本框(Text)", "基础控件-文本框-text--通用"],
     ["单选框(Radio)", "基础控件-单选框-radio--单选框-radio"],
     ["滚动条(Scrollbar)", "基础控件-滚动条-scrollbar--滚动条-scrollbar"],
     ["下拉框(Selector)", "基础控件-下拉框-selector--下拉框-selector"],
-    ["联想文本框(suggest)", "基础控件-联想文本框-suggest--联想文本框-suggest"],
-    ["多行文本框(Textarea)", "基础控件-多行文本框-textarea--多行文本框-textarea"],
+    ["联想文本框(Suggest)", "基础控件-联想文本框-suggest--联想文本框-suggest"],
+    [
+        "多行文本框(Textarea)",
+        "基础控件-多行文本框-textarea--多行文本框-textarea",
+    ],
     ["栅格(Grid)", "基础控件-栅格-grid--基础"],
-    ["动画(Animate)", ""],
+    ["动画(Animate)", "基础控件-动画-animate--动画-animate"],
     ["轮播图(Carousel)", "复用组件-轮播图-carousel--通用"],
-    ["日期时间选择(Datepicker)", "复用组件-日期时间选择-datepicker--日期时间选择-datepicker"],
+    [
+        "日期时间选择(Datepicker)",
+        "复用组件-日期时间选择-datepicker--日期时间选择-datepicker",
+    ],
     ["表单(Form)", "复用组件-表单-form--表单-form"],
     ["弹层(Layer)", "复用组件-弹层-layer--通用"],
     ["灯箱(Lightbox)", "复用组件-灯箱-lightbox--iframe模式"],
@@ -31,11 +38,20 @@ const LABEL_URI_MAP = new Map([
     ["选项卡(Tab)", "复用组件-选项卡-tab--通用"],
     ["文件上传(Upload)", "复用组件-文件上传-upload--文件上传-upload"],
     ["图像放大镜(Zoom)", "复用组件-图像放大镜-zoom--图像放大镜-zoom"],
-    ["图片充满居中(Adapimage)", "常用函数-图片充满居中-adapimage--图片充满居中-adapimage"],
+    [
+        "图片充满居中(Adapimage)",
+        "常用函数-图片充满居中-adapimage--图片充满居中-adapimage",
+    ],
     ["数字自增效果(Numup)", "常用函数-数字自增效果-numup--数字自增效果-numup"],
-    ["响应式文本截断(Omit)", "常用函数-响应式文本截断-omit--响应式文本截断-omit"],
+    [
+        "响应式文本截断(Omit)",
+        "常用函数-响应式文本截断-omit--响应式文本截断-omit",
+    ],
     ["响应式图片(Picture)", "常用函数-响应式图片-picture--响应式图片-picture"],
-    ["图片懒加载(Lazyload)", "复用组件-图片懒加载-lazyload--图片懒加载-lazyload"],
+    [
+        "图片懒加载(Lazyload)",
+        "复用组件-图片懒加载-lazyload--图片懒加载-lazyload",
+    ],
     ["工具函数(Utils)", "常用函数-工具函数-utils--page"],
 ]);
 function activate(context) {
@@ -44,9 +60,13 @@ function activate(context) {
     }));
     treeView_1.TreeViewProvider.initTreeViewItem();
     context.subscriptions.push(vscode.commands.registerCommand("itemClick", (label) => {
-        // vscode.window.showInformationMessage(label);
-        const webView = webView_1.createWebView(context, vscode.ViewColumn.Active, LABEL_URI_MAP.get(label), label);
-        context.subscriptions.push(webView);
+        if (label != "基础控件" &&
+            label != "复用组件" &&
+            label != "常用函数" &&
+            label != "HopeUI") {
+            const webView = webView_1.createWebView(context, vscode.ViewColumn.Active, LABEL_URI_MAP.get(label), label);
+            context.subscriptions.push(webView);
+        }
     }));
 }
 exports.activate = activate;
--- src/treeView.js
+++ src/treeView.js
@@ -4,38 +4,43 @@ exports.TreeViewProvider = exports.TreeItemNode = void 0;
 /*
  * @Author       : Evan.G
  * @Date         : 2020-12-24 15:39:21
- * @LastEditTime : 2021-01-18 16:11:23
+ * @LastEditTime : 2021-01-28 18:07:23
  * @Description  :
  */
 const vscode_1 = require("vscode");
 const path_1 = require("path");
 const ITEM_ICON_MAP = new Map([
-    ["按钮(Button)", "icon.svg"],
-    ["复选框(Checkbox)", "icon.svg"],
-    ["文本框(Text)", "icon.svg"],
-    ["单选框(Radio)", "icon.svg"],
-    ["滚动条(Scrollbar)", "icon.svg"],
-    ["下拉框(Selector)", "icon.svg"],
-    ["联想文本框(suggest)", "icon.svg"],
-    ["多行文本框(Textarea)", "icon.svg"],
-    ["栅格(Grid)", "icon.svg"],
-    ["动画(Animate)", "icon.svg"],
-    ["轮播图(Carousel)", "icon.svg"],
-    ["日期时间选择(Datepicker)", "icon.svg"],
-    ["表单(Form)", "icon.svg"],
-    ["弹层(Layer)", "icon.svg"],
-    ["灯箱(Lightbox)", "icon.svg"],
-    ["流式加载(Loadmore)", "icon.svg"],
-    ["分页(Pager)", "icon.svg"],
-    ["选项卡(Tab)", "icon.svg"],
-    ["文件上传(Upload)", "icon.svg"],
-    ["图像放大镜(Zoom)", "icon.svg"],
-    ["图片充满居中(Adapimage)", "icon.svg"],
-    ["数字自增效果(Numup)", "icon.svg"],
-    ["响应式文本截断(Omit)", "icon.svg"],
-    ["响应式图片(Picture)", "icon.svg"],
-    ["图片懒加载(Lazyload)", "icon.svg"],
-    ["工具函数(Utils)", "icon.svg"],
+    ["获取最新版", "icon.svg"],
+    ["按钮 [Button]", "controls.svg"],
+    ["复选框 [Checkbox]", "controls.svg"],
+    ["文本框 [Text]", "controls.svg"],
+    ["单选框 [Radio]", "controls.svg"],
+    ["滚动条 [Scrollbar]", "controls.svg"],
+    ["下拉框 [Selector]", "controls.svg"],
+    ["联想文本框 [Suggest]", "controls.svg"],
+    ["多行文本框 [Textarea]", "controls.svg"],
+    ["栅格 [Grid]", "controls.svg"],
+    ["动画 [Animate]", "controls.svg"],
+    ["轮播图 [Carousel]", "components.svg"],
+    ["日期时间选择 [Datepicker]", "components.svg"],
+    ["表单 [Form]", "components.svg"],
+    ["弹层 [Layer]", "components.svg"],
+    ["灯箱 [Lightbox]", "components.svg"],
+    ["流式加载 [Loadmore]", "components.svg"],
+    ["分页 [Pager]", "components.svg"],
+    ["选项卡 [Tab]", "components.svg"],
+    ["文件上传 [Upload]", "components.svg"],
+    ["图像放大镜 [Zoom]", "components.svg"],
+    ["图片充满居中 [Adapimage]", "utils.svg"],
+    ["数字自增效果 [Numup]", "utils.svg"],
+    ["响应式文本截断 [Omit]", "utils.svg"],
+    ["响应式图片 [Picture]", "utils.svg"],
+    ["图片懒加载 [Lazyload]", "utils.svg"],
+    ["工具函数 [Utils]", "utils.svg"],
+    ["基础控件", "controls.svg"],
+    ["复用组件", "components.svg"],
+    ["常用函数", "utils.svg"],
+    ["HopeUI", "version.svg"],
 ]);
 class TreeItemNode extends vscode_1.TreeItem {
     constructor(label, collapsibleState) {
@@ -60,33 +65,56 @@ class TreeViewProvider {
         return element;
     }
     getChildren(element) {
-        return [
-            "按钮(Button)",
-            "复选框(Checkbox)",
-            "文本框(Text)",
-            "单选框(Radio)",
-            "滚动条(Scrollbar)",
-            "下拉框(Selector)",
-            "联想文本框(suggest)",
-            "多行文本框(Textarea)",
-            "栅格(Grid)",
-            "轮播图(Carousel)",
-            "日期时间选择(Datepicker)",
-            "表单(Form)",
-            "弹层(Layer)",
-            "灯箱(Lightbox)",
-            "流式加载(Loadmore)",
-            "分页(Pager)",
-            "选项卡(Tab)",
-            "文件上传(Upload)",
-            "图像放大镜(Zoom)",
-            "图片充满居中(Adapimage)",
-            "数字自增效果(Numup)",
-            "响应式文本截断(Omit)",
-            "响应式图片(Picture)",
-            "图片懒加载(Lazyload)",
-            "工具函数(Utils)"
-        ].map((item) => new TreeItemNode(item, vscode_1.TreeItemCollapsibleState.None));
+        if (element) {
+            console.log(element);
+            if (element.label == "基础控件") {
+                console.log("基础控件");
+                return [
+                    "按钮 [Button]",
+                    "复选框 [Checkbox]",
+                    "文本框 [Text]",
+                    "单选框 [Radio]",
+                    "滚动条 [Scrollbar]",
+                    "下拉框 [Selector]",
+                    "联想文本框 [Suggest]",
+                    "多行文本框 [Textarea]",
+                    "栅格 [Grid]",
+                    "动画 [Animate]",
+                ].map((item) => new TreeItemNode(item, vscode_1.TreeItemCollapsibleState.None));
+            }
+            else if (element.label == "复用组件") {
+                console.log("复用组件");
+                return [
+                    "轮播图 [Carousel]",
+                    "日期时间选择 [Datepicker]",
+                    "表单 [Form]",
+                    "弹层 [Layer]",
+                    "灯箱 [Lightbox]",
+                    "流式加载 [Loadmore]",
+                    "分页 [Pager]",
+                    "选项卡 [Tab]",
+                    "文件上传 [Upload]",
+                    "图像放大镜 [Zoom]",
+                ].map((item) => new TreeItemNode(item, vscode_1.TreeItemCollapsibleState.None));
+            }
+            else if (element.label == "常用函数") {
+                console.log("常用函数");
+                return [
+                    "图片充满居中 [Adapimage]",
+                    "数字自增效果 [Numup]",
+                    "响应式文本截断 [Omit]",
+                    "响应式图片 [Picture]",
+                    "图片懒加载 [Lazyload]",
+                    "工具函数 [Utils]",
+                ].map((item) => new TreeItemNode(item, vscode_1.TreeItemCollapsibleState.None));
+            }
+            else if (element.label == "HopeUI") {
+                return ["获取最新版"].map((item) => new TreeItemNode(item, vscode_1.TreeItemCollapsibleState.None));
+            }
+        }
+        else {
+            return ["基础控件", "复用组件", "常用函数"].map((item) => new TreeItemNode(item, vscode_1.TreeItemCollapsibleState.Expanded));
+        }
     }
     static initTreeViewItem() {
         const treeViewProvider = new TreeViewProvider();
